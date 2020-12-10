5f39474d42c605ff59f08b3c2eebb92842510eeb
Manish Chugh <manishchugh1981@gmail.com>
2020-12-10 15:39:58 -0500

    latest code Dec 10

---
+++ src/main/java/SeleniumSessions/DifferentLocators.java
@@ -0,0 +1,61 @@
+//**** P1-Dec 10**
+package SeleniumSessions;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DifferentLocators {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		//driver.get("https://www.orangehrm.com/orangehrm-30-day-trial/");
+		//Thread.sleep(6000);
+		
+		//1. id:unique
+		
+		//2. name : not unique
+		//driver.findElement(By.name("FirstName")).sendKeys("Manish");
+		
+		//3. xpath -> is not an attribute, its a locator(address of the element in DOM)
+		//driver.findElement(By.xpath("//*[@id=\"Form_submitForm_FirstName\"]")).sendKeys("Manish");
+		
+		//4. css selector -> not an attribute, its a locator
+		//driver.findElement(By.cssSelector("#Form_submitForm_LastName")).sendKeys("Manish");
+		
+		//5. linkText: only for links:having <a> html tag
+		//driver.findElement(By.linkText("ES")).click();
+		
+		//6. partialLinkText: only for links -> long link text
+		//driver.findElement(By.partialLinkText("Privacy")).click();
+		
+		//7. tagName -> used to get the elements on a page, like total No. of Links, images
+//		driver.get("https://www.freework.com");
+//		Thread.sleep(6000);
+//		
+//		String h2_text=driver.findElement(By.tagName("h2")).getText();
+//		System.out.println(h2_text);
+//		
+//		String text=driver.findElement(By.linkText("Sign up")).getText();
+//		System.out.println(text);
+		
+		//8. className : not unique
+		driver.get("https://authentication.td.com/uap-ui/index.html?consumer=easyweb&locale=en_CA#/login/easyweb-getting-started");
+		Thread.sleep(6000);
+		
+		driver.findElement(By.className("ng-pristine ng-valid form-control ng-empty ng-untouched")).sendKeys("123345345");
+		
+		//form-control private-form__control login-email
+		//form-control private-form__control login-password m-bottom-3
+		
+		//username -> ng-pristine ng-valid form-control ng-empty ng-untouched
+		//password -> otp-always-show-error ng-pristine ng-untouched ng-valid form-control ng-empty
+	}
+	
+
+}
--- src/main/java/SeleniumSessions/LocatorsConcept.java
+++ src/main/java/SeleniumSessions/LocatorsConcept.java
@@ -1,6 +1,8 @@
 //** Dec 3-P2***
 package SeleniumSessions;
 
+import java.sql.Driver;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -9,11 +11,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class LocatorsConcept {
-
+	static WebDriver driver;
+	
 	public static void main(String[] args) throws InterruptedException {
 		
 		WebDriverManager.chromedriver().setup();	
-		WebDriver driver=new ChromeDriver();
+		driver=new ChromeDriver();
 		
 		driver.get("https://www.orangehrm.com/orangehrm-30-day-trial/");
 		//Creating the webelement + action(click, sendkeys, gettext)
@@ -28,18 +31,64 @@ public class LocatorsConcept {
 		
 		//2nd way
 		
-		WebElement FName=driver.findElement(By.id("Form_submitForm_FirstName"));
-		WebElement LName=driver.findElement(By.id("Form_submitForm_LastName"));
-		WebElement Submit_btn=driver.findElement(By.id("Form_submitForm_action_request"));
+//		WebElement FName=driver.findElement(By.id("Form_submitForm_FirstName"));
+//		WebElement LName=driver.findElement(By.id("Form_submitForm_LastName"));
+//		WebElement Submit_btn=driver.findElement(By.id("Form_submitForm_action_request"));
+//		
+//		FName.sendKeys("Manish");
+//		FName.sendKeys("Tenveer");
+//		LName.sendKeys("Chugh");
+//		
+//		Submit_btn.click();
 		
-		FName.sendKeys("Manish");
-		FName.sendKeys("Tenveer");
-		LName.sendKeys("Chugh");
+		//3rd way
+//		By FName=By.id("Form_submitForm_FirstName");
+//		By LName=By.id("Form_submitForm_LastName");
+//		By Submit_btn=By.id("Form_submitForm_action_request");
 		
-		Submit_btn.click();
+//		WebElement FName_ele=driver.findElement(FName);
+//		WebElement LName_ele=driver.findElement(LName);
+//		WebElement Submit_btn_ele=driver.findElement(Submit_btn);
+//		
+//		FName_ele.sendKeys("Manish");
+//		LName_ele.sendKeys("Chugh");
+//		Submit_btn_ele.click();
 		
+		//4th
+//		getElement(FName).sendKeys("Manish");
+//		getElement(LName).sendKeys("Chugh");
+//		getElement(Submit_btn).click();
 		
+		//5th
+//		doSendKeys(FName, "Manish");
+//		doSendKeys(LName, "Chugh");
+//		doClick(Submit_btn);
+		
+		//6th
+		
+		String FName_id="Form_submitForm_FirstName";
+		String LName_id="Form_submitForm_LastName";
+		String Submit_btn_id="Form_submitForm_action_request";
+		
+		doSendKeys(getLocator(FName_id), "Manish");
+		doSendKeys(getLocator(LName_id), "Chugh");
+		doClick(getLocator(Submit_btn_id));
 		
 	}
+		public static By getLocator(String value) {
+			return By.id(value);
+		}
+	
+		public static WebElement getElement(By locator) {
+			return driver.findElement(locator);
+		}
+		
+		public static void doSendKeys(By locator, String value) {
+			getElement(locator).sendKeys(value);
+		}
+		
+		public static void doClick(By locator) {
+			getElement(locator).click();
+		}
 
 }
